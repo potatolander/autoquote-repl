e7fe3061069ec7c1d12c04f40227202dcf52f285
Akhil Pillai <111009970+akpi816218@users.noreply.github.com>
2022-08-11 15:55:21 +0000

    renamed repl; updated discord.jsv14.2.0

--- commands/allquotes.js
+++ commands/allquotes.js
@@ -10,7 +10,7 @@ module.exports = {
 	async execute(interaction) {
 		await interaction.reply(bold(`Viewer discretion advised.\nBy following the link, you agree that you will ${italic('not press charges against anyone for anything')} caused by your following the link. For the safety of your brain cells and sanity, there will be a delay of approximately 10 seconds until you will be given the link to the quotebag.`));
     setTimeout(async () => {
-      await interaction.followUp('If you still want your brain screwed over, you can view the quotebag at https://AutoQuote.IndigoMotorcycl.repl.co/allquotes.');
+      await interaction.followUp(`If you still want your brain screwed over, you can view the quotebag ${hyperlink('here', 'https://AutoQuote.IndigoMotorcycl.repl.co/allquotes')}.`);
     }, 10000);
 	}
 };
